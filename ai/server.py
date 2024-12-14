ab21828ac0638273f66ac5f4a94c254491e80103
xuhe <xuhe114514@gmail.com>
2024-12-14 15:27:27 +0800

    feat: add api to handle file

--- ai/server.py
+++ ai/server.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi import File, UploadFile
 
 app = FastAPI()
 
@@ -8,3 +9,8 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+@app.post("/handle_picture/")
+async def handle_picture(file: UploadFile = File(...)):
+    # Do something with the file
+    return {"filename": file.filename}
