4bcfbc480253e6f202ae53f1d72c18910ebf51d0
xuhe <xuhe114514@gmail.com>
2024-12-14 17:10:54 +0800

    feat: add cors and Root api

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"log"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/xuhe2/intelligence-software-expr3/controllers"
 )
 
 func main() {
-    log.Println("Hello, World!")
+	r := gin.Default()
+	// CORS
+	// 配置 CORS 中间件
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // 允许访问的域名
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	r.GET("/",controllers.Root)
+
+	r.Run(":3001") // 监听并在 0.0.0.0:3001 上启动服务
 }
